f9bb97ad09d3715b9a08070ac9901fd08ac8a73a
Serkan Kaya <serkankaya0721@gmail.com>
2025-03-03 20:04:13 +0100

    split admin layout into structured partial views.

--- Project.UserInterface/Controllers/AdminLayoutController.cs
+++ Project.UserInterface/Controllers/AdminLayoutController.cs
@@ -8,5 +8,40 @@ namespace Project.UserInterface.Controllers
 		{
 			return View();
 		}
-	}
+
+		public PartialViewResult HeadPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult PreloaderPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult NavheaderPartial()
+		{
+			return PartialView();
+		}
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+		public PartialViewResult SidebarPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult FooterPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult ScriptPartial()
+		{
+			return PartialView();
+		}
+    }
 }
---
+++ Project.UserInterface/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.UserInterface.Controllers
+{
+	public class StaffController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
