b47e16fcde3e20d8937d2ff0435efcc015c3eaf9
Serkan Kaya <serkankaya0721@gmail.com>
2025-03-03 19:39:32 +0100

    implemented admin layout and added AdminController.

---
+++ Project.UserInterface/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.UserInterface.Controllers
+{
+	public class AdminLayoutController : Controller
+	{
+		public IActionResult _AdminLayout()
+		{
+			return View();
+		}
+	}
+}
